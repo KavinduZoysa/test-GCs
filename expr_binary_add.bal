e671f54b5cac12e65764611718a76a021a850da4
kavindu <kavindu@wso2.com>
2021-06-05 13:35:49 +0530

    Add sources

---
+++ expr_binary_add.bal
@@ -0,0 +1,3 @@
+public function foo(int x, int y) returns int {
+    return  x + y;
+}
---
+++ test.c
@@ -0,0 +1,6 @@
+extern void foo(long, long);
+
+int main() {
+    foo(9223372036854775807, 1);
+    return 0;
+}
